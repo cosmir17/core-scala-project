212a223db6980a4b6ae91529bd47e268f56ca28f
cosmir17 <ks1537@gmail.com>
2021-07-05 23:27:54 +0100

    readable test for supermarket test

--- src/test/scala/com/sk/supermarket/SupermarketTest.scala
+++ src/test/scala/com/sk/supermarket/SupermarketTest.scala
@@ -6,27 +6,27 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class SupermarketTest extends AnyFunSuite with Matchers {
 
-  test("A B C D") {
+  test("Buying A, B, C & D") {
     val result = Supermarket.calculateTotalPrice(List(A, B, C, D))
     result shouldBe 115
   }
 
-  test("3A B C D") {
+  test("Buying 3As, B, C & D") {
     val result = Supermarket.calculateTotalPrice(List(A, A, A, B, C, D))
     result shouldBe 195
   }
 
-  test("3A 2B C D") {
+  test("Buying 3As, 2Bs, C & D") {
     val result = Supermarket.calculateTotalPrice(List(A, A, A, B, C, D, B))
     result shouldBe 210
   }
 
-  test("empty case") {
+  test("Buying empty case") {
     val result = Supermarket.calculateTotalPrice(List())
     result shouldBe 0
   }
 
-  test("C D") {
+  test("Buying C & D") {
     val result = Supermarket.calculateTotalPrice(List(C, D))
     result shouldBe 35
   }
