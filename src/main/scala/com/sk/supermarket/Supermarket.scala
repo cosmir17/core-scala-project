c5b09bfa26b7d121f5d0250777d878919b927d13
cosmir17 <ks1537@gmail.com>
2021-07-06 23:59:38 +0100

    refactoring Supermarket.scala

--- src/main/scala/com/sk/supermarket/Supermarket.scala
+++ src/main/scala/com/sk/supermarket/Supermarket.scala
@@ -3,30 +3,26 @@ package com.sk.supermarket
 object Supermarket {
   case class Promotion(count: Int, newPrice: Int)
 
-  sealed abstract class Item(val price: Int, val rule: Option[Promotion])
+  sealed abstract class Item(val price: Int, val promotion: Option[Promotion] = None)
   case object A extends Item(50, Some(Promotion(3, 130)))
   case object B extends Item(30, Some(Promotion(2, 45)))
-  case object C extends Item(20, None)
-  case object D extends Item(15, None)
+  case object C extends Item(20)
+  case object D extends Item(15)
 
   /**
     * calculate the total price of a list of items
     * @param items
-    * @tparam real items
     * @return price
     */
   def calculateTotalPrice(items: List[Item]): Int = items
       .groupBy(identity)
-      .map { case (k, v) => (k, v.size) }
-      .map { case (k, v) => applyPromotion(k, v) }
+      .map { case (item, listOfItems) => (item.price, listOfItems.size, item.promotion) }
+      .map {
+        case (price, count, Some(promotion)) => applyPromotion(price, count, promotion)
+        case (price, count, None) => price * count
+         }
       .sum
 
-  private def applyPromotion(item: Item, count: Int) = item.rule.map(t => {
-    val d = count / t.count
-    val remainder = count % t.count
-
-    val specialP = d * t.newPrice
-    val normalP = remainder * item.price
-    specialP + normalP
-  }).getOrElse(item.price * count)
+  private def applyPromotion(price: Int, count: Int, promotion: Promotion) =
+    (count / promotion.count * promotion.newPrice) + (count % promotion.count * price)
 }
